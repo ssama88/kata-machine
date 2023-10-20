5443e7b44f62407e1f1ccb1064c7486cf5a94fa2
Saket Sama <ssama@777part.com>
2023-10-20 15:57:16 -0700

    two crystal balls implementation

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,21 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
-
+    const interval = Math.floor(Math.sqrt(breaks.length))
+    let goodPoint = interval,
+    foundTrue = false
+    while(goodPoint < breaks.length){
+        if(foundTrue){
+            if(breaks[goodPoint]) return goodPoint
+            goodPoint ++
+        }else{
+            if(breaks[goodPoint]){ //go back to the last known false point where we can then increment by 1 till we find the first true point
+                foundTrue = true 
+                goodPoint -= interval
+            }else{
+            goodPoint += interval
+    
+            }
+        }
+      
+    }
+    return -1
 }
