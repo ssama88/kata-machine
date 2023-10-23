2ce3d4d1605771cc74d09712e7f2f0c3d0230826
Saket Sama <ssama@777part.com>
2023-10-22 23:24:48 -0700

    working stack implementation

--- src/day1/Stack.ts
+++ src/day1/Stack.ts
@@ -1,18 +1,41 @@
+type QNode<T> = {
+    value: T;
+    prev?: QNode<T>;
+};
 export default class Stack<T> {
-    public length: number;
+    public length: number = 0;
+    private head?: QNode<T>;
 
-    
-
-    constructor() {
-    }
+    constructor() {}
 
     push(item: T): void {
+        ++this.length;
 
-}
+        if (!this.head) {
+            this.head = this.createNode(item);
+            return;
+        }
+        const head = this.head;
+        this.head = this.createNode(item);
+        this.head.prev = head;
+    }
     pop(): T | undefined {
+        if (!this.head) return undefined;
+        --this.length;
 
-}
+        if (this.length === 0) {
+            const head = this.head;
+            this.head = undefined;
+            return head.value;
+        }
+        const head = this.head;
+        this.head = this.head.prev;
+        return head.value;
+    }
     peek(): T | undefined {
-
-}
+        return this.head?.value;
+    }
+    private createNode(item: T): QNode<T> {
+        return { value: item };
+    }
 }
