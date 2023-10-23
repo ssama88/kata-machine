969ca9c3871603f432eca3a37c7fa9a7b9d162fb
Saket Sama <ssama@777part.com>
2023-10-22 21:38:13 -0700

    working bubble sort

--- src/day1/BubbleSort.ts
+++ src/day1/BubbleSort.ts
@@ -1,3 +1,10 @@
 export default function bubble_sort(arr: number[]): void {
-
+    for(let j = 0; j<arr.length; j++ )
+    for(let i = 0; i< arr.length-1-j; i++){
+        if(arr[i]> arr[i+1]){
+            let tempVal = arr[i]
+            arr[i] = arr[i+1]
+            arr[i+1] = tempVal
+        }
+    }
 }
