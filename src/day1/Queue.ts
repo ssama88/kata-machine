4046a3d8b8acbd90880e677e9d04e142185b978b
Saket Sama <ssama@777part.com>
2023-10-22 22:40:20 -0700

    working queue implementation

--- src/day1/Queue.ts
+++ src/day1/Queue.ts
@@ -1,18 +1,36 @@
+type QNode<T> = {
+    value: T;
+    next?: QNode<T>;
+};
 export default class Queue<T> {
-    public length: number;
-
-    
+    public length: number = 0;
+    private head?: QNode<T>;
+    private tail?: QNode<T>;
 
     constructor() {
+        this.head = this.tail = undefined;
     }
 
     enqueue(item: T): void {
+        const node = { value: item } as QNode<T>;
+        if (!this.tail) {
+            this.tail = this.head = node;
+        } else {
+            this.tail.next = node;
+            this.tail = node;
+        }
 
-}
+        this.length++;
+    }
     deque(): T | undefined {
-
-}
+        if (!this.head) return undefined;
+        const tempHead = this.head;
+        this.head = this.head.next;
+        this.length--;
+        if (this.length === 0) this.tail = undefined;
+        return tempHead?.value;
+    }
     peek(): T | undefined {
-
-}
+        return this.head?.value;
+    }
 }
