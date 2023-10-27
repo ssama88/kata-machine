5902c775e7fdb86853e9b0b330696cefc5140bf9
Saket Sama <ssama@777part.com>
2023-10-26 17:23:47 -0700

    qorking quick sort implementation

--- src/day1/QuickSort.ts
+++ src/day1/QuickSort.ts
@@ -1,3 +1,26 @@
+const recurseSort = (array: number[], low: number, high: number): void => {
+    if (low >= high) {
+        return;
+    }
+    const pivotIdx = partition(array, low, high);
+    recurseSort(array, low, pivotIdx - 1);
+    recurseSort(array, pivotIdx + 1, high);
+};
+function partition(array: number[], low: number, high: number): number {
+    const pivot = array[high];
+    let idx = low - 1;
+    for (let i = low; i < high; i++) {
+        if (array[i] <= pivot) {
+            idx++;
+            const temp = array[i];
+            array[i] = array[idx];
+            array[idx] = temp;
+        }
+    }
+    array[high] = array[idx + 1];
+    array[idx + 1] = pivot;
+    return idx + 1;
+}
 export default function quick_sort(arr: number[]): void {
-
+    recurseSort(arr, 0, arr.length - 1);
 }
